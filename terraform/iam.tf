a12f01b691577793c4e99dca144ab3774ae1a8dd
Christian Steinhaus <christian.steinhaus@rwth-aachen.de>
2023-01-29 20:05:11 +0100

    IAM: move policy into datablock

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,36 +1,35 @@
-resource "aws_iam_user" "github_action_anwarias_cloudchallenge" {
+resource "aws_iam_user" "github_action_user" {
   name = "github-action-anwarias-cloudchallenge"
 }
 
-resource "aws_iam_group" "github_action_cloudchallenge" {
-  name = "github_action_cloudchallenge"
+resource "aws_iam_group" "github_action_group" {
+  name = "github-action-anwarias-cloudchallenge"
 }
 
-resource "aws_iam_group_membership" "assignment" {
-  name  = "assignment"
-  users = [aws_iam_user.github_action_anwarias_cloudchallenge.name]
-  group = aws_iam_group.github_action_cloudchallenge.name
+resource "aws_iam_group_membership" "github_action_policyassignment" {
+  name  = "github-action-policyassignment"
+  users = [aws_iam_user.github_action_user.name]
+  group = aws_iam_group.github_action_group.name
 }
 
-resource "aws_iam_group_policy" "github_action_anwarias_cloudchallenge" {
-  name  = "github_action_anwarias_cloudchallenge"
-  group = aws_iam_group.github_action_cloudchallenge.name
-  policy = jsonencode({
-    Version = "2012-10-07"
-    Statement = [
-      {
-        Action = [
-          "s3:ListBucket",
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:DeleteObject"
-        ]
-        Effect = "Allow"
-        Resource = [
-          "arn:aws:s3:::${var.databucket}/*",
-          "arn:aws:s3:::${var.databucket}"
-        ]
-      }
+data "aws_iam_policy_document" "github_action_policy" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject"
     ]
-  })
+    resources = [
+        aws_s3_bucket.databucket.arn,
+        "${aws_s3_bucket.databucket.arn}/*"
+    ]
+    effect    = "Allow"
+  }
+}
+
+resource "aws_iam_group_policy" "github_action_policy" {
+  name   = "github-action-anwarias-cloudchallenge_policy"
+  group  = aws_iam_group.github_action_group.name
+  policy = data.aws_iam_policy_document.github_action_policy.json
 }
