8e66dc5a03e0f36d04e6e236dc4318d32bfec5e6
Christian Steinhaus <christian.steinhaus@rwth-aachen.de>
2023-01-29 18:36:17 +0100

    Split TF files into smaller units

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,56 +1,15 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.0"
-        }
-    }
-    backend "s3" {
-        bucket = "anwarias-cloudchallenge-terraform-state"
-        key = "tf-infra/terraform.tfstate"
-        dynamodb_table = "terraform-state-locking"
-        encrypt = true
-        region = "eu-central-1"
-  }
-
-}
-
-provider "aws" {
-    region = "eu-central-1"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-    bucket = "anwarias-cloudchallenge-terraform-state"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket_acl" "private_bucket" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl = "private"
-}
-
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name = "anwarias-cloudchallenge-terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
+  backend "s3" {
+    bucket         = "anwarias-cloudchallenge-terraform-state"
+    key            = "tf-infra/terraform.tfstate"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+    region         = "eu-central-1"
   }
 }
