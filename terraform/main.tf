8a9a58f2510766131d4194c2fee90c309969cbc7
Christian Steinhaus <christian.steinhaus@rwth-aachen.de>
2023-01-29 16:57:04 +0100

    Add terraform lock db

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "anwarias_cloudchallenge_terraform_state"
+    bucket = "anwarias-cloudchallenge-terraform-state"
     force_destroy = true
 }
 
@@ -34,5 +34,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
     apply_server_side_encryption_by_default {
       sse_algorithm     = "AES256"
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "anwarias-cloudchallenge-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
